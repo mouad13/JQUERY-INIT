e75b292e59cb65e3c12c50f212da011f9e4e273f
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-28 12:32:12 +0200

    Step 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 // Step 1
-$( "#removeDiv").click(function() {
+$("#removeDiv").click(function() {
 	$("#remove").slideUp();
 
-
 });
+
+// Step 2
+var text = "" ;
+$("#setText").keypress(function(){
+	text = $("#setText").val();
+	$("#needText").text(text);
+});
+
+
+
+
+
+
+
+
+// Step 3
